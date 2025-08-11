5d8a2d97e565b3033becd61724f34595553d43aa
smaq92 <145607578+smaq92@users.noreply.github.com>
2025-08-10 23:18:37 -0600

    fix the homepage and add 404 page

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, FlatListComponent, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { PRODUCTS } from '../../../assets/products'
+import ProductListItem from '../../components/product-list-item'
+import ListHeader from '../../components/list-header'
+
 
 const index = () => {
   return (
     // View is just like div and is used for grouping
     <View>
-      <Text>Shop Home</Text>
+      {/* react native component for lists that we can get from API */}
+      <FlatList data={PRODUCTS} renderItem={({item})=>
+      <ProductListItem product={item}/>}
+      keyExtractor={item => item.id.toString()}
+      numColumns={2}
+      ListHeaderComponent={ListHeader}
+      contentContainerStyle={styles.flatListContent}
+      columnWrapperStyle={styles.flatListColumn}
+      style={{paddingHorizontal: 10, paddingVertical: 5}}
+      />
     </View>
   )
 }
 
 export default index
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+flatListContent: {
+  paddingBottom: 20,
+},
+flatListColumn: {
+  justifyContent: "space-between"
+}
+
+})
