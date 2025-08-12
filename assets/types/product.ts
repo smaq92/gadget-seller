8112cb9ff0357ad4fe4d3e2340573c871ce6d4e1
smaq92 <145607578+smaq92@users.noreply.github.com>
2025-08-11 23:53:49 -0600

    set up dynamic category pages

--- assets/types/product.ts
+++ assets/types/product.ts
@@ -11,3 +11,14 @@ export type Product = {
   category: Omit<Category, 'products'>;
   maxQuantity: number;
 };
+
+// export type Product = {
+//   id: number;                              // A number (e.g., 1, 2, 3)
+//   title: string;                           // Text (e.g., "Red T-shirt")
+//   slug: string;                            // Text used in URLs (e.g., "red-tshirt")
+//   imagesUrl: ImageSourcePropType[];        // An array of image sources for React Native
+//   price: number;                           // Numeric price (e.g., 19.99)
+//   heroImage: ImageSourcePropType;          // A single main image
+//   category: Omit<Category, 'products'>;    // A Category object, but WITHOUT its 'products' property
+//   maxQuantity: number;                     // The maximum number you can buy
+// };
